2799409cf97d83ccde4ea35551d37cff0ca706a3
Shuo LI <shuo.li@ias.u-psud.fr>
2015-06-05 17:50:25 +0200

    new version get photo

--- get_photo_tumblr.py
+++ get_photo_tumblr.py
@@ -35,63 +35,69 @@ def getContent(url,type):
         return dataStr 
 
 def baidu_tieba(url,current_page='/page/1',x=1):
-    if int(current_page.split('/')[-1])<=end_page:       
-        if url == bdurl:
-            dataStr = getContent(url+current_page,"html") 
-            reg_img = r"""img.+src\s*=\s*"(\S+)\.(\w+)"(data-highres|\s+)"""
-        '''
-        else:
-            dataStr = getContent(url,"html")
-            reg_img = r"""a\s+href\s*=\s*"?(\S+)\.(\w+)"""          
-        '''
-        urlre_img = re.compile(reg_img)
-        imglist = urlre_img.findall(dataStr)
-        for imgurl in imglist:
-            print imgurl  
-            if imgurl[1]!='com':
-                imgtype=imgurl[1]
-                imgurl =''.join('%s.%s' % (imgurl[0],imgtype))
-                #print imgurl
-                if imgurl.find('http:')>=0:
-                    print "\tdownloading: %s,%s" % (x,imgurl)
-                    imgdata=getContent(imgurl,"img")
-                    if imgdata!='EOF':
-                        outputFile = 'ai/%s.%s.%s' % (current_page.split('/')[-1],x,imgtype)
-                        #print x
-                        f = open(outputFile,'wb')
-                        try:
+    #if int(current_page.split('/')[-1])<=end_page:       
+    if url == bdurl:
+        dataStr = getContent(url+current_page,"html") 
+        reg_img = r"""img\salt=?"?\s?.+src\s*=\s*"(\S+)\.(\w+)".+data-highres"""
+    '''
+    else:
+        dataStr = getContent(url,"html")
+        reg_img = r"""a\s+href\s*=\s*"?(\S+)\.(\w+)"""          
+    '''
+    urlre_img = re.compile(reg_img)
+    imglist = urlre_img.findall(dataStr)
+    for imgurl in imglist:
+        print imgurl  
+        if imgurl[1]!='com':
+            imgtype=imgurl[1]
+            imgurl =''.join('%s.%s' % (imgurl[0],imgtype))
+            #print imgurl
+            if imgurl.find('http:')>=0:
+                print "\tdownloading: %s,%s" % (x,imgurl)
+                imgdata=getContent(imgurl,"img")
+                if imgdata!='EOF':
+                    outputFile = 'ai/%s.%s.%s' % (current_page.split('/')[-1],x,imgtype)
+                    #print x
+                    f = open(outputFile,'wb')
+                    try:
 
-                            f.write(imgdata)
-                        except:
-                            print "*****"    
-                        f.close()
-                        x = x + 1   
+                        f.write(imgdata)
+                    except:
+                        print "*****"    
+                    f.close()
+                    x = x + 1   
            
         
-        reg_link=r"""src\s*=\s*"?(http://dailycatdrawings\.tumblr\.com/post/\S+/false)"""
-        urlre_link = re.compile(reg_link)  
-        linklist=urlre_link.findall(dataStr)
-        for link in linklist:
-            #print link
-            x=baidu_tieba(link,current_page,x)
+    reg_link=r"""src\s*=\s*"?(http://dailycatdrawings\.tumblr\.com/post/\S+/false)"""
+    urlre_link = re.compile(reg_link)  
+    linklist=urlre_link.findall(dataStr)
+    for link in linklist:
+        print "***************************************"
+        x=baidu_tieba(link,current_page,x)
 
-        reg_nextpage = r"""href\s*=\s*"?(/page/\d+)"""  
-        urlre_nextpage = re.compile(reg_nextpage)
-        nextpage=urlre_nextpage.findall(dataStr)
-        print nextpage
-        if nextpage!=[]:
-            if nextpage[0] == nextpage[-1] and nextpage[0] != "/page/2":
-                print "Finished! :)"
+    reg_nextpage = r"""href\s*=\s*"?(/page/\d+)"""  
+    urlre_nextpage = re.compile(reg_nextpage)
+    nextpage=urlre_nextpage.findall(dataStr)
+    print nextpage
+    if nextpage!=[]:
+        if nextpage[0] == nextpage[-1] and nextpage[0] != "/page/2":
+            print "Finished! :)"
+        else:
+            print nextpage[0].split('/')[-1] + "  " + nextpage[-1].split('/')[-1]
+            if int(nextpage[0].split('/')[-1]) > int(nextpage[-1].split('/')[-1]):
+            	x=baidu_tieba(url,nextpage[0])
             else:
-                x=baidu_tieba(url,nextpage[-1])            
+            	print "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+            	x=baidu_tieba(url,nextpage[-1])            
         
-        return x
+    return x
 """
 bdurl = str(raw_input(u'URL:\n'))  
 begin_page = int(raw_input(u'START PAGE:\n'))  
 end_page = int(raw_input(u'END PAGE:\n'))  
 """
-bdurl='http://dailycatdrawings.tumblr.com'
+bdurl='http://eeekou.tumblr.com'
 start_page=1
-end_page=75
+#end_page=75
 baidu_tieba(bdurl) 
+
